6f5b89527cc6e2f4a8ff2c3b7dcbc802b77f9b07
杨圣 <526884815@qq.com>
2026-08-08 11:22:46 +0800

    add empty project to git

---
+++ rag_demo/src/test/java/com/jk/ai/rag_demo/RagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.ai.rag_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
